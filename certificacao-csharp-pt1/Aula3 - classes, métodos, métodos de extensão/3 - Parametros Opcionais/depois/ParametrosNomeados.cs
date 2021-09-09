75d131575bcea18d0e6b8d2f24e2bd80ee1cd649
matheus.menecucci@domvsit.com.br <matheus.menecucci@domvsit.com.br>
2021-09-09 16:40:58 -0300

    Adicionar arquivos de projeto.

---
+++ certificacao-csharp-pt1/Aula3 - classes, métodos, métodos de extensão/3 - Parametros Opcionais/depois/ParametrosNomeados.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class ParametrosNomeados : IAulaItem
+    {
+        public void Executar()
+        {
+            //chamando o método normal passando os arg na ordem
+            ImprimirDetalhesDoPedido("Maria", 31,"Caneca Vermelha");
+
+            //chamando o método e passando os parametros em qualquer ordem, pois eu dei nomes aos argumentos
+            ImprimirDetalhesDoPedido(numeroPedido: 31, nomeProduto: "Caneca Vermelha", vendedor: "Maria" );
+            
+            //Argumentos nomeados misturados com argumentos posicionais são válidos
+            ImprimirDetalhesDoPedido("Maria de Fátima", 31, nomeProduto: "Caneca Vermelha");
+           
+                
+            // As 2 linhas abaixo geravam erro de compilação ANTES DO C# 7.2:
+            ImprimirDetalhesDoPedido(vendedor: "Maria de Fátima", 31, nomeProduto: "Caneca Vermelha");
+            ImprimirDetalhesDoPedido("Maria de Fátima", numeroPedido: 31, "Caneca Vermelha");
+        
+        }
+
+        void ImprimirDetalhesDoPedido(string vendedor, int numeroPedido, string nomeProduto)
+        {
+            if (string.IsNullOrWhiteSpace(vendedor))
+            {
+                throw new ArgumentException(message: "Nome de vendedor não pode ser nulo ou vazio.", paramName: nameof(vendedor));
+            }
+
+            Console.WriteLine($"Vendedor: {vendedor}, Pedido No.: {numeroPedido}, Produto: {nomeProduto}");
+        }
+    }
+}
---
+++ certificacao-csharp-pt1/Aula3 - classes, métodos, métodos de extensão/4 - Métodos de Extensão/depois/MetodosDeExtensao.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class MetodosDeExtensao : IAulaItem
+    {
+        public void Executar()
+        {
+            // \r\ é quebra de linha
+            Impressora impressora = new Impressora("Este é\r\no meu documento");
+
+            impressora.ImprimirDocumento();
+
+            ImprimirDocumentoHtml(impressora.Documento);
+
+            //extensão
+            impressora.ExtensaoImprimirDocumentoHtml();
+        }
+         void ImprimirDocumentoHtml(string documento)
+        {
+            Console.WriteLine($"<html><body>{documento}</body></html>");
+        }
+       
+    }
+
+    class Impressora
+    {
+        public string Documento { get; }
+
+        public Impressora(string documento)
+        {
+            this.Documento = documento;
+        }
+
+        public void ImprimirDocumento()
+        {
+            Console.WriteLine();
+            Console.WriteLine(Documento);
+        }
+        //public void ImprimirDocumentoHtml()
+        //{
+        //    Console.WriteLine($"<html><body>{Documento}</body></html>");
+        //}
+       
+    }
+
+    //criando um método de extensão
+    static class ImpressoraExtensions
+    {   
+        //com o this, eu estou estendendo a minha classe impressora com o método ImprimirDocumentoHtml
+        public static void ExtensaoImprimirDocumentoHtml(this Impressora impressora)
+        {
+            Console.WriteLine($"<html><body>{impressora.Documento} EM HTML</body></html>");
+        }
+       
+    }
+}
+
+
