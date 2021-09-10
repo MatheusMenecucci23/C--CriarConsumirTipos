698e578b5ffabe58ef36f422482b7f91d3d5c2c7
matheus.menecucci@domvsit.com.br <matheus.menecucci@domvsit.com.br>
2021-09-10 09:00:32 -0300

    Adicionar arquivos de projeto.

---
+++ certificacao-csharp-pt1/Aula5 - box ou unbox para converter entre tipos de valor/1 - Boxing/depois/Boxing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class Boxing : IAulaItem
+    {
+        public void Executar()
+        {
+            int numero = 57; 
+             
+            //fazendo um boxing: colocando o número dentro da caixa
+            object caixa = numero;
+
+            Console.WriteLine(caixa);
+
+            //concatenando
+            Console.WriteLine(string.Concat("Resposta", numero, true));
+        }
+    }
+}
---
+++ certificacao-csharp-pt1/Aula5 - box ou unbox para converter entre tipos de valor/2 - Unboxing/depois/Unboxing.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class Unboxing : IAulaItem
+    {
+        public void Executar()
+        {
+            int numero = 57;      // tipo de valor
+
+            object caixa = numero; // boxing
+
+            try
+            {
+                int unboxed = (int)caixa;
+
+                System.Console.WriteLine("Unboxing OK.");
+            }
+            catch (System.InvalidCastException e)// quando o cast der errado entre nesse e bloco e atribua o erro ao "e"
+            {
+                System.Console.WriteLine("{0} Erro: Unboxing incorreto", e.Message);
+            }
+
+          
+
+        }
+    }
+}
