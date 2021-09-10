57b21883ec9ed9866dde2228506e974badc08199
matheus.menecucci@domvsit.com.br <matheus.menecucci@domvsit.com.br>
2021-09-10 17:47:45 -0300

    Adicionar arquivos de projeto.

---
+++ certificacao-csharp-pt1/Aula7 - lidar com tipos dinâmicos/1 - string, var, object e dynamic/depois/UsandoDynamic.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class UsandoDynamic : IAulaItem
+    {
+        public void Executar()
+        {
+            //tipo é definido na compilação
+            string s = "Certificação C#";
+            //tipo é definido na compilação (por inferência)
+            var v = "Certificação C#";
+            //tipo é definido na compilação
+            object o = "Certificação C#";
+
+            Console.WriteLine(s);
+            Console.WriteLine(v);
+            Console.WriteLine(o);
+
+            //transformando em uppercase
+            s = s.ToUpper();
+            v = v.ToUpper();
+            //convertendo o O em string a aí transformando em upercase
+            o = ((string)o).ToUpper();
+
+            Console.WriteLine(s);
+            Console.WriteLine(v);
+            Console.WriteLine(o);
+
+            //s = 123;
+            //v = 123;
+            //só o objeto pode receber o valor do tipo int
+            o = 123;
+
+            //fazendo o cast para o O par conseguir somar
+            o = (int)o + 4;
+
+            Console.WriteLine(o);
+
+            //tipo é definido somente na execução
+            //tipo dinamico pode-se atribuir qualquer tipo de variável
+            dynamic d = "Certificação C#";
+            Console.WriteLine(d);
+            d = d.ToUpper();
+            Console.WriteLine(d);
+            d = 123;
+            Console.WriteLine(d);
+            d = d + 4;
+            Console.WriteLine(d);
+        }
+    }
+}
---
+++ certificacao-csharp-pt1/Aula7 - lidar com tipos dinâmicos/2 - Conversões entre dynamic e outros/depois/ConversoesDeDynamic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class ConversoesDeDynamic : IAulaItem
+    {
+        public void Executar()
+        {
+            //dymanic pode receber qualquer tipo de variável
+            dynamic d1 = 7;
+            dynamic d2 = "essa é uma string qualquer";
+            dynamic d3 = DateTime.Now;
+            dynamic d4 = new List<int>() { 123, 456, 789 };
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+
+            //eu posso converter um dynamic para um tipo estatico, desde que o tipo seja compatível
+            int i = d1;
+            string str = d2;
+            DateTime dt = d3;
+            List<int> lista = d4;
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+
+            //Erro de tempo de execução! não é possível converter string em int
+            //int inteiro = d2;
+        }
+    }
+}
